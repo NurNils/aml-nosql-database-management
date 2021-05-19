a4e6f5162e3b19bbbf5f11ccaf8655cee0ef1d34
NamidM <namid.marxen@gmail.com>
2021-05-19 19:53:25 +0200

    Added first login frame

--- SOURCE/FRONTEND/aml-database-management/src/app/app.component.ts
+++ SOURCE/FRONTEND/aml-database-management/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { environment as env } from '../environments/environment';
+import { ApiService } from './services/api/api.service';
 import { LanguageService } from './services/language/language.service';
 
 @Component({
@@ -9,8 +8,18 @@ import { LanguageService } from './services/language/language.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  /** Only for testing -> Check if user is logged in */
+  public login: boolean = false;
+
   /** Constructor */
-  constructor(languageService: LanguageService) {
+  constructor(languageService: LanguageService, private apiService: ApiService) {
     languageService.initializeI18n();
+
+    this.authorize();
+  }
+
+  /** Authorize user */
+  async authorize() {
+    const res = await this.apiService.login();
   }
 }
